3b7c8ad9d5023e6ea22a7ab8bb20b0ad5c734aa6
cole0995 <cole0995@bu.edu>
2017-11-25 18:14:22 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
-# place in /etc/rc.local with filepath: .../healthyhands.py &
+# place in /etc/rc.local with filepath: .../healthyhands.py
 
-(sleep 10; python healthyhands.py)
+(sleep 10; python /filepath/healthyhands.py)&
