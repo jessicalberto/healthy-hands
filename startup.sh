0290f8d1533e2eaecf2241f1ba25ced425a95a06
cole0995 <cole0995@bu.edu>
2017-11-25 18:18:33 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,11 @@
 # place in /etc/rc.local with filepath: .../healthyhands.py
 
-(sleep 10; python /filepath/healthyhands.py)&
+sleep 10
+
+/usr/bin/tvservice -o               # disable HDMI
+
+echo none | sudo tee /sys/class/leds/led0/trigger # Set the Pi Zero ACT LED trigger to 'none'.
+
+echo 1 | sudo tee /sys/class/leds/led0/brightness # Turn off the Pi Zero ACT LED.
+
+python /filepath/healthyhands.py&   # run program
