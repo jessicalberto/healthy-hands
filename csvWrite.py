e6835713643043c93022c89cfd4e65608dbe7bac
Anthony Pasquariello <antpas@bu.edu>
2018-02-13 15:36:25 -0500

    Update csvWrite.py

--- csvWrite.py
+++ csvWrite.py
@@ -4,12 +4,22 @@ import datetime
 
 outputFile = "C:\Users\\antth\PycharmProjects\csvTest\\" + datetime.datetime.now().strftime("%m_%d_%Y") + ".csv"
 
+soapNum = 4
+waterNum = 1
+date = datetime.datetime.now().strftime("%m_%d_%Y")
+
 def writeToCSV():
     if os.path.exists(outputFile) is True:
-       print("write to file")
+        with open(outputFile, 'ab') as outcsv:
+             writer = csv.writer(outcsv)
+             writer.writerow([date, waterNum, soapNum])
+        print("Wrote to file")
+
     else:
         with open(outputFile, 'wb') as outcsv:
             writer = csv.writer(outcsv)
             writer.writerow(['Date-Time', 'Number of Water Events', 'Number of Soap Events'])
-        print("Now write to file")
+
+        print("Created file")
+
 writeToCSV()
