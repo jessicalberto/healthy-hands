a8c218f151a6b19451ce7003d0f5b2c3fd1f9395
Jessica Alberto <koualoha@bu.edu>
2018-02-14 20:35:39 +0000

    Updated system.py
    Added rotations per second, fixed File Folder creation and saving into the right location, fixed .csv overriding when new line is saved, added print statements for readability, removed checkPath function, added headers in .csv when file is created

--- system.py
+++ system.py
@@ -34,30 +34,28 @@ timeCount = 0
 soapCount = 0
 debounceTime = 0
 
-def checkPath():
-    currentDatePath = "/home/pi/Documents/Data/" + "OutputData.csv"
-    if(os.path.exists(currentDatePath) is True):
-        return True
-    else:
-        os.mkdir(currentDatePath)
-        return False
+
     
-def writeToCSV(soapNum):
+def writeToCSV(soapNum, rotations):
+    
     if os.path.exists(outputFile) is True:
         
-        date = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
-        waterNum = 1
-        with open(outputFile, 'w') as outcsv:
-             writer = csv.writer(outcsv)
-             writer.writerow([date, waterNum, soapNum])
-        print("Wrote to file")
+        if startRecording is True:
+            return;
+        else:
+            date = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+            waterNum = 1
+            with open(outputFile, 'a') as outcsv:
+                 writer = csv.writer(outcsv)
+                 writer.writerow([date, waterNum, soapNum, rotations/10])
+            print("Wrote to file Output.csv")
 
     else:
         with open(outputFile, 'w') as outcsv:
             writer = csv.writer(outcsv)
-            writer.writerow(['Date-Time'.encode(), 'Number of Water Events'.encode(), 'Number of Soap Events'.encode()])
+            writer.writerow(['Date-Time', 'Number of Water Events', 'Number of Soap Events', 'Rotations/sec'])
 
-        print("Created file")
+        print("Created new file Output.csv")
 
 global flowCount
 flowCount = 0
@@ -67,19 +65,28 @@ def countPulse(channel):
     global waterOn
     global filename
     global timeCount
+    global startRecording
+    startRecording = False
     flowCount = flowCount + 1
     print(flowCount)
     
     if(flowCount <= 1):
-        checkPath()
         waterOn = True
+        startRecording = True
         print("Water Sensor On")
         timeCount = time.time();
-        print(timeCount);
+        writeToCSV(0, 0);
+        startRecording = False
+        print(datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S"))
         recordCount = 1
-        camera.start_recording(date + '.h264')
-        print("Camera started")
         filename = date + '.h264'
+        savePath = "/home/pi/Documents/Data/" + datetime.datetime.now().strftime("%m_%d_%Y")
+        if not os.path.exists(savePath):
+            os.makedirs(savePath)
+        completeVideo = os.path.join(savePath, filename)
+        camera.start_recording(completeVideo)
+        print("Camera started")
+        
     
 GPIO.add_event_detect(FLOW_SENSOR, GPIO.FALLING, callback = countPulse)
     
@@ -101,10 +108,11 @@ while True:
                       
     if((waterOn is True) and (time.time() > timeCount + 10)):
         print("Stopped camera recording")
+        print("Total number of rotations:")
         print(flowCount)
+        rotations=flowCount
         camera.stop_recording()
-        savePath = "/home/pi/Documents/Data/" + datetime.datetime.now().strftime("%m_%d_%Y")
-        completeVideo = os.path.join(savePath, filename)
+        
         print("Water Sensor Stopped")
         recordCount = 0
         prevSoap = OFF
@@ -115,10 +123,11 @@ while True:
         prevWater = 0
         timeCount = 0
         date = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
-        writeToCSV(soapCount)
+        writeToCSV(soapCount,rotations)
         print("Soap Count: ")
-        print(soapCount);
-        print(time.time());
+        print(soapCount)
+        print("Time:")
+        print(date)
         soapCount = 0
         
 
@@ -126,4 +135,3 @@ print("Camera Done")
 print("Soap Count: ")
 print(soapCount);
         
-
